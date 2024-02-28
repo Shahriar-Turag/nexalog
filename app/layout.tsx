9a8d528f00177a7586df9055988d580bd66a59d6
Shahriar Turag <shahriarturag.st@gmail.com>
2024-02-28 20:30:11 +0600

    isomorphical error solved

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,9 @@
 'use client';
-import { siteConfig } from '@/config/siteConfig';
 import { cn } from '@/lib/utils';
 import '@/styles/globals.css';
-import type { Metadata } from 'next';
+
 import { Roboto } from 'next/font/google';
 import { Providers } from './Provider';
-import AppTopNavigation from '@/components/layout-components/app-top-navigation';
-import { useRouter } from 'next/router';
 
 const fontRoboto = Roboto({
 	subsets: ['latin'],
@@ -14,28 +11,6 @@ const fontRoboto = Roboto({
 	variable: '--font-roboto',
 	weight: ['100', '300', '400', '500', '700'],
 });
-
-export const metadata: Metadata = {
-	title: { default: siteConfig.name, template: `%s | ${siteConfig.name}` },
-	description: siteConfig.description,
-	// TODO: need to add the keywords (search)
-	keywords: [''],
-	authors: [
-		{
-			name: 'Nexalog',
-			url: 'url here',
-		},
-	],
-	creator: 'Team Nexalog',
-	openGraph: {
-		type: 'website',
-		locale: 'en_US',
-		url: siteConfig.url,
-		title: siteConfig.name,
-		description: siteConfig.description,
-		siteName: siteConfig.name,
-	},
-};
 
 interface RootLayoutProps {
 	children: React.ReactNode;
